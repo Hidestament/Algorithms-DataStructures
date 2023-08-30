c8ebf5cc9fb390a381349abc05013de31861e160
Hidestament <snhrhdt@gmail.com>
2023-08-30 14:08:10 +0000

    add binary indexed tree

---
+++ DataStructures/RangeTree/binary_indexed_tree.py
@@ -0,0 +1,164 @@
+from typing import Optional
+
+
+class BinaryIndexedTree:
+    """部分和 + 一点更新を O(log N)で行う
+
+    Method:
+        - get(i): A[i]を取得
+        - add(i, x): A[i] += x
+        - update(i, x): A[i] = xに更新する
+        - sum(i): A[0..i)の総和
+        - sum_range(i, j): A[i..j)の総和
+        - lower_bound(x): A[0] + A[1] + ... A[i - 1] >= x となる最小のiを取得
+    """
+    def __init__(self, N: int = 10**6):
+        """Binary Indexed Tree. 0で初期化する. 0-indexedだが, 内部では1-indexedで扱う.
+
+        Args:
+            N (int): 配列の要素数. Defaults to 10**6.
+        """
+        self.size = N + 1
+        self.tree = [0] * self.size
+
+    def __getitem__(self, i: int) -> int:
+        """A[i]を取得.
+
+        Args:
+            i (int): index. 0-indexed.
+
+        Returns:
+            int: A[i]
+
+        TimeComplexity:
+            O(log N)
+        """
+        return self.get(i)
+
+    def get(self, i: int) -> int:
+        """A[i]を取得.
+
+        Args:
+            i (int): index. 0-indexed.
+
+        Returns:
+            int: A[i]
+
+        TimeComplexity:
+            O(log N)
+        """
+        if not (0 <= i < self.size):
+            raise IndexError("list index out of range")
+
+        return self.sum(i + 1) - self.sum(i)
+
+    def add(self, i: int, x: int):
+        """A[i] += x
+
+        Args:
+            i (int): index. 0-indexed.
+            x (int): 加算する値.
+
+        TimeComplexity:
+            O(log N)
+        """
+        if i < 0:
+            return
+
+        i += 1
+        while i < self.size:
+            # print(f"{i=}")
+            self.tree[i] += x
+            # 真上の位置は, iにiのLSBを加えたモノ
+            i += i & -i
+
+    def update(self, i: int, x: int):
+        """A[i] = xに更新する
+
+        Args:
+            i (int): index. 0-indexed.
+            x (int): 更新値.
+
+        TimeComplexity:
+            O(log N)
+        """
+        if i < 0:
+            return
+
+        self.add(i, -self.get(i) + x)
+
+    def sum(self, i: int) -> int:
+        """A[0..i)の総和
+
+        Args:
+            i (int): index. 0-indexed. 含まない.
+
+        Returns:
+            int: sum(A[:i])
+
+        TimeComplexity:
+            O(log N)
+        """
+        if i <= 0:
+            return 0
+
+        i = min(i, self.size - 1)
+        s = 0
+        while i > 0:
+            s += self.tree[i]
+            i -= i & -i
+
+        return s
+
+    def sum_range(self, i: int, j: int) -> int:
+        """A[i..j)の総和
+
+        Args:
+            i (int): index. 0-indexed. 含む.
+            j (int): index. 0-indexed. 含まない
+
+        Returns:
+            int: sum(A[i:j])
+
+        TimeComplexity:
+            O(log N)
+        """
+        if i >= j:
+            return 0
+        return self.sum(j) - self.sum(i)
+
+    def lower_bound(self, x: int) -> Optional[int]:
+        """A[0] + A[1] + ... A[i - 1] >= x となる最小のiを取得
+
+        Args:
+            x (int): lower bound.
+
+        Returns:
+            Optional[int]: index. 0-indexed. 含まない. 該当するものがない場合はNone.
+
+        TimeComplexity:
+            O(log N)
+
+        Note:
+            各項は非負である必要がある
+        """
+        if x <= 0:
+            return 0
+
+        if self.sum(self.size) < x:
+            return None
+
+        # 現在見ている区間の範囲
+        length = self.size
+        s = 0
+        i = 0
+        while length > 0:
+            # 現在の区間を足してもxに届かない場合, その区間を加えて右の子へ
+            if (i + length < self.size) and self.tree[i + length] + s < x:
+                s += self.tree[i + length]
+                i += length
+
+            # 1つ下の階層へ (区間は半分になる)
+            length //= 2
+
+        return i + 1
---
+++ tests/DataStructures/RangeTree/test_binary_indexed_tree.py
@@ -0,0 +1,152 @@
+from itertools import combinations
+
+from DataStructures.RangeTree.binary_indexed_tree import BinaryIndexedTree
+
+
+def test_add():
+    A = [1, 5, 2, 3, 7, 5, 6, 4]
+    bit = BinaryIndexedTree(len(A))
+
+    for i, a in enumerate(A):
+        bit.add(i, a)
+
+    assert bit.tree == [
+        0,
+        A[0],
+        A[0] + A[1],
+        A[2],
+        A[0] + A[1] + A[2] + A[3],
+        A[4],
+        A[4] + A[5],
+        A[6],
+        A[0] + A[1] + A[2] + A[3] + A[4] + A[5] + A[6] + A[7]
+    ]
+
+
+def test_sum():
+    bit = BinaryIndexedTree(10)
+
+    A = [1, 5, 2, 3, 4, 5, 6, 7, 4, 2]
+    for i, a in enumerate(A):
+        bit.add(i, a)
+
+    assert bit.sum(-1) == 0
+    assert bit.sum(0) == 0
+    assert bit.sum(5) == 1 + 5 + 2 + 3 + 4
+    assert bit.sum(len(A)) == sum(A)
+    assert bit.sum(len(A) + 1) == sum(A)
+    assert bit.sum(1000) == sum(A)
+
+    for i in range(1, len(A) + 1):
+        assert bit.sum(i) == sum(A[:i])
+
+
+def test_sum_range():
+    bit = BinaryIndexedTree(10)
+
+    A = [1, 5, 2, 3, 4, 5, 6, 7, 4, 2]
+    for i, a in enumerate(A):
+        bit.add(i, a)
+
+    # A[1..9) = [5, 2, 3, 4, 5, 6, 7, 4]
+    assert bit.sum_range(1, 9) == 5 + 2 + 3 + 4 + 5 + 6 + 7 + 4
+    assert bit.sum_range(1, 1) == 0
+    assert bit.sum_range(1, 0) == 0
+
+    assert bit.sum_range(-1, 10) == sum(A)
+    assert bit.sum_range(-10, 1000) == sum(A)
+
+    for i, j in combinations(range(len(A) + 1), r=2):
+        assert bit.sum_range(i, j) == sum(A[i:j])
+
+
+def test_get():
+    A = [1, 5, 2, 3, 7, 5, 6, 4]
+    bit = BinaryIndexedTree(len(A))
+
+    for i, a in enumerate(A):
+        bit.add(i, a)
+
+    assert bit.get(0) == A[0]
+    assert bit[0] == A[0]
+
+    assert bit.get(1) == A[1]
+    assert bit[1] == A[1]
+
+    assert bit.get(2) == A[2]
+    assert bit[2] == A[2]
+
+    for i, a in enumerate(A):
+        assert bit.get(i) == a
+        assert bit[i] == a
+
+
+def test_update():
+    A = [1, 5, 2, 3, 7, 5, 6, 4]
+    bit = BinaryIndexedTree(len(A))
+
+    for i, a in enumerate(A):
+        bit.add(i, a)
+
+    for i in range(len(A)):
+        bit.update(i, 100)
+
+    assert bit[0] == 100
+    assert bit[1] == 100
+
+    for i in range(len(A)):
+        assert bit[i] == 100
+
+
+def test_lower_bound():
+    A = [1, 5, 2, 3, 7, 5, 6, 4]
+    bit = BinaryIndexedTree(len(A))
+
+    for i, a in enumerate(A):
+        bit.add(i, a)
+
+    # A = [1, 5, 2, 3, 7, 5, 6, 4]
+    # Sum(A) = [1, 6, 8, 11, 18, 23, 29, 33]
+    assert bit.lower_bound(-1) == 0
+    assert bit.lower_bound(0) == 0
+    assert bit.lower_bound(1) == 1
+    assert bit.lower_bound(5) == 2
+    assert bit.lower_bound(6) == 2
+
+    # A[0] + A[1] = 1 + 5 = 6
+    # A[0] + A[1] + A[2] = 1 + 5 + 2 = 8
+    assert bit.lower_bound(7) == 3
+    assert bit.lower_bound(8) == 3
+
+    # A[0] + A[1] + A[2] = 1 + 5 + 2 = 8
+    # A[0] + A[1] + A[2] + A[3] = 1 + 5 + 2 + 3 = 11
+    assert bit.lower_bound(9) == 4
+    assert bit.lower_bound(10) == 4
+    assert bit.lower_bound(11) == 4
+
+    # A[0] + A[1] + A[2] + A[3] = 1 + 5 + 2 + 3 = 11
+    # A[0] + A[1] + A[2] + A[3] + A[4] = 1 + 5 + 2 + 3 + 7 = 18
+    assert bit.lower_bound(12) == 5
+    assert bit.lower_bound(17) == 5
+    assert bit.lower_bound(18) == 5
+
+    # A[0] + A[1] + A[2] + A[3] + A[4] = 1 + 5 + 2 + 3 + 7 = 18
+    # A[0] + A[1] + A[2] + A[3] + A[4] + A[5] = 1 + 5 + 2 + 3 + 7 + 5 = 23
+    assert bit.lower_bound(19) == 6
+    assert bit.lower_bound(22) == 6
+    assert bit.lower_bound(23) == 6
+
+    # A[0] + ... + A[5] = 23
+    # A[0] + ... + A[5] + A[6] = 23 + 6 = 29
+    assert bit.lower_bound(24) == 7
+    assert bit.lower_bound(28) == 7
+    assert bit.lower_bound(29) == 7
+
+    # A[0] + ... + A[6] = 23 + 6 = 29
+    # A[0] + ... + A[6] + A[7] = 23 + 6 + 4 = 33
+    assert bit.lower_bound(30) == 8
+    assert bit.lower_bound(32) == 8
+    assert bit.lower_bound(33) == 8
+
+    assert bit.lower_bound(34) is None
+    assert bit.lower_bound(10000) is None
