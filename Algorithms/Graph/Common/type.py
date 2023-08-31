d69820ae8d4d89026ce5e3b15bb5471e96b488cc
Hidestament <snhrhdt@gmail.com>
2023-08-31 12:11:08 +0000

    add type

--- Algorithms/Graph/Common/type.py
+++ Algorithms/Graph/Common/type.py
@@ -1,2 +1,5 @@
 # ex. v0 -e0- v1 -e1- v2 = [[(v0, e0)], [(v0, e0), (v2, e1)], [(v1, e1)]]
 AdjacencyListWithEdgeNumber = list[list[tuple[int, int]]]
+
+# ex. v0 -e0- v1 -e1- v2 = [(v0, v1), (v1, v2)]
+EdgeList = list[tuple[int, int]]
--- Algorithms/Graph/CycleDetection/undirected_cycle_detection_by_union_find.py
+++ Algorithms/Graph/CycleDetection/undirected_cycle_detection_by_union_find.py
@@ -1,15 +1,16 @@
+from Algorithms.Graph.Common.type import EdgeList
 from DataStructures.DisjointSet.union_find_tree import UnionFindTree
 
 
 def undirected_cycle_detection_by_uf(
     num_vertex: int,
-    edges: list[tuple[int, int]],
+    edges: EdgeList,
 ) -> bool:
     """単純無向グラフにおいて閉路が存在するかどうかを判定する. UnionFindTreeを使用. 復元はできない.
 
     Args:
         num_vertex (int): 頂点の個数N.
-        edges (list[tuple[int, int]]): 辺のリスト. 0-indexed.
+        edges (EdgeList): 辺のリスト. 0-indexed.
 
     Returns:
         bool: True -> 閉路が存在する, False -> 閉路が存在しない
