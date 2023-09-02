420ffb56be38a07f02c5ee349962f1652085c9f9
Hidestament <snhrhdt@gmail.com>
2023-09-02 01:30:54 +0000

    solve LibraryChecker - PointAddRangeSum

---
+++ LibraryChecker/DataStructures/PointAddRangeSum/solve_by_seg_tree.py
@@ -0,0 +1,24 @@
+# https://judge.yosupo.jp/submission/159028
+
+import sys
+
+from DataStructures.RangeTree.segment_tree import SegmentTree
+
+
+input = sys.stdin.readline
+
+N, Q = map(int, input().split())
+A = list(map(int, input().split()))
+
+seg = SegmentTree(N, lambda x, y: x + y, 0)
+for i, a in enumerate(A):
+    seg[i] = a
+
+for _ in range(Q):
+    query = list(map(int, input().split()))
+    if query[0] == 0:
+        p, x = query[1:]
+        seg.add(p, x)
+    else:
+        left, right = query[1:]
+        print(seg.query(left, right))
