64e1fb3c92e4fe920e8630775fb4efdf05d0cf17
Hidestament <snhrhdt@gmail.com>
2023-09-03 14:58:05 +0000

    solve AOJ

---
+++ AOJ/DSL/2A_RangeMinimumQuery/solve_seg_tree.py
@@ -0,0 +1,20 @@
+# https://onlinejudge.u-aizu.ac.jp/status/users/hidexchan/submissions/1/DSL_2_A/judge/8240441/PyPy3
+
+import sys
+
+from DataStructures.RangeTree.segment_tree import RangeMinimumQuery
+
+
+input = sys.stdin.readline
+
+N, Q = map(int, input().split())
+seg = RangeMinimumQuery([2 ** 31 - 1 for _ in range(N)])
+
+for _ in range(Q):
+    query = list(map(int, input().split()))
+    if query[0] == 0:
+        i, x = query[1:]
+        seg.update(i, x)
+    else:
+        left, right = query[1:]
+        print(seg.query(left, right + 1))
---
+++ AOJ/DSL/2B_RangeSumQuery/solve_seg_tree.py
@@ -0,0 +1,20 @@
+# https://onlinejudge.u-aizu.ac.jp/status/users/hidexchan/submissions/1/DSL_2_B/judge/8240445/PyPy3
+
+import sys
+
+from DataStructures.RangeTree.segment_tree import RangeSumQuery
+
+
+input = sys.stdin.readline
+
+N, Q = map(int, input().split())
+seg = RangeSumQuery([0 for _ in range(N)])
+
+for _ in range(Q):
+    query = list(map(int, input().split()))
+    if query[0] == 0:
+        i, x = query[1:]
+        seg.add(i - 1, x)
+    else:
+        left, right = query[1:]
+        print(seg.query(left - 1, right))
